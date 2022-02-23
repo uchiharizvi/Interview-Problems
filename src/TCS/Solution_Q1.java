63a4c4e1ad0713b13895caaf251060c1a7949603
Kavish Rizvi <rizvikavish205.com>
2022-02-23 20:07:33 +0530

    TCS Solution

--- src/TCS/Solution_Q1.java
+++ src/TCS/Solution_Q1.java
@@ -1,10 +1,22 @@
 package TCS;
 
+import java.util.Arrays;
+import java.util.Scanner;
+/**Inconsistent Solution - Improvement Required**/
 public class Solution_Q1 {
     public static void main(String[] args) {
-        int arr[] = {9, 6, 3, 8, 5};
-        int res[] = new int[2];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Size of Array : ");
+        int n = scan.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter element value of array[] : ");
+        for(int i=0;i<n;i++) {
+            System.out.println("Enter value for Element " + i);
+            arr[i] = scan.nextInt();
+        }
+        Arrays.stream(arr).forEach(System.out::println);
         int len = arr.length;
+        int res[] = new int[2];
         res[0] = arr[0];
         res[1] = arr[1];
         for (int i = 2; i < len; i++) {
--- src/TCS/Solution_Q2.java
+++ src/TCS/Solution_Q2.java
@@ -1,4 +1,17 @@
 package TCS;
 
 public class Solution_Q2 {
+    public static void main(String[] args) {
+        String a = "Hello World";
+        int len = a.length()-1;
+        char[] c = a.toCharArray();
+        reverse(c,len);
+
+    }
+    static void reverse(char[] c, int len){
+        if(len ==0) return;
+        System.out.print(c[len]);
+        len--;
+        reverse(c,len);
+    }
 }
